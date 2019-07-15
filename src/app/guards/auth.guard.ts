f018179d6b31695191e9f6faed5e8b0ecd7639b5
S-Chenn <supernovachin@gmail.com>
2019-07-16 01:19:51 +0900

    追加

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -9,14 +9,23 @@ import { User } from 'firebase';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(public afAuth: AngularFireAuth) {}
+  constructor(
+    public afAuth: AngularFireAuth,
+    private router: Router
+    ) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.afAuth.authState.pipe(
-      map((afUser: User | null) => !!afUser)
+      map((afUser: User | null) => {
+        if(afUser){
+          return true;
+        } else {
+          this.router.navigateByUrl('/login');
+          return false;
+        }
+      })
     );
   }
-
 }
