d04ad34047315adb60c4ff5b6db42b3ce6d0f619
S-Chenn <supernovachin@gmail.com>
2019-07-16 01:26:23 +0900

    ゲストガードを実装

--- src/app/guards/guest.guard.ts
+++ src/app/guards/guest.guard.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-
+import { AngularFireAuth } from '@angular/fire/auth';
+import { map } from 'rxjs/operators';
+import { User } from 'firebase';
 @Injectable({
   providedIn: 'root'
 })
 export class GuestGuard implements CanActivate {
+  constructor(
+    public afAuth: AngularFireAuth,
+    private router: Router
+    ) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.afAuth.authState.pipe(
+      map((afUser: User | null) => {
+        if(!afUser){
+          return true;
+        } else {
+          this.router.navigateByUrl('/');
+          return false;
+        }
+      })
+    );
   }
-  
 }
