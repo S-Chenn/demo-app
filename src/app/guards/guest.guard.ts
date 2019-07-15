3b86f331357ebb2736b879089db6dc7a49ac9a9d
S-Chenn <supernovachin@gmail.com>
2019-07-16 01:13:41 +0900

    login logout後のリダイレクト処理を実装

--- src/app/guards/guest.guard.ts
+++ src/app/guards/guest.guard.ts
@@ -1,22 +1,15 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { AngularFireAuth } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { User } from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
-  constructor(public afAuth: AngularFireAuth) {}
-
+export class GuestGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.afAuth.authState.pipe(
-      map((afUser: User | null) => !!afUser)
-    );
+    return true;
   }
-
+  
 }
