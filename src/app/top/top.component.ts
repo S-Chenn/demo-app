3859ed77fb0d15777b18452da1446856666e11e8
S-Chenn <supernovachin@gmail.com>
2019-07-16 23:51:22 +0900

    createroomを実装

--- src/app/top/top.component.ts
+++ src/app/top/top.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ChatService } from '../services/chat.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-top',
@@ -10,7 +13,10 @@ export class TopComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private chatService: ChatService
+    ) { }
 
   ngOnInit() {
     this.form = this.fb.group({
@@ -25,7 +31,7 @@ export class TopComponent implements OnInit {
 
   createRoom() {
     if(this.form.valid) {
-    console.log(this.form.value);
+    this.chatService.createRoom(this.form.value.name);
     }else {
     alert("ルーム名を入力してください。");
     }
