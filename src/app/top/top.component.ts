6be1f78dbe69b243efda94559751740c1d7bac99
S-Chenn <supernovachin@gmail.com>
2019-07-16 22:55:20 +0900

    ルーム作成フォームを実装

--- src/app/top/top.component.ts
+++ src/app/top/top.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-top',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      name: ['', [
+        Validators.required,
+        Validators.maxLength(16),
+        Validators.minLength(4)
+      ]]
+    })
   }
 
+  createRoom() {
+    if(this.form.valid) {
+    console.log(this.form.value);
+    }else {
+    alert("ルーム名を入力してください。");
+    }
+  }
 }
